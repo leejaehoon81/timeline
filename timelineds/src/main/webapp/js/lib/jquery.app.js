e767c0e68188e9d558a2c8cb91e1b40770e47683
bit <bit@bit-PC>
2015-10-12 15:01:28 +0900

    구경하기 적용

--- timelineds/src/main/webapp/js/lib/jquery.app.js
+++ timelineds/src/main/webapp/js/lib/jquery.app.js
@@ -21,15 +21,15 @@ topSection.css('height', windowHeight);
 Smooth Scroll To Anchor
 =============================================== */
 //jQuery for page scrolling feature - requires jQuery Easing plugin
-/*$(function() {
-    $('.navbar-nav a').bind('click', function(event) {
+$(function() {
+    $('#example').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top - 0
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-});*/
+});
 /* ==============================================
 Preloader
 =============================================== */
