9095bad5332ee773b83938be47edbcb5fd81a0e2
bit <bit@bit-PC>
2015-10-16 10:30:00 +0900

    .

--- timelineds/src/main/webapp/js/app/myMain.js
+++ timelineds/src/main/webapp/js/app/myMain.js
@@ -3,9 +3,7 @@ define([ 'jquery', 'handlebars' ], function($, handlebars) {
     init : function() {
       $.getJSON('mainList.do', function(result) {
         var source = $('#chroTemplate').html();
-        var template = handlebars.compile(source, {
-          ignoreStandalone : true
-        });
+        var template = handlebars.compile(source);
         var content = template(result);
         $('.haveBox').html(content);
       });
